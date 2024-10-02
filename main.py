d91e92e08fd7ca86743da05e17470872617d4bde
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-10-02 09:33:24 -0300

    ead dataset and dummy team, pos, name

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+import pandas as pd
+
+dataset = pd.read_csv('./assets/dataset.csv')
+
+import pandas as pd
+
+dataset = pd.read_csv('./assets/dataset.csv')
+
+performance_dummies = pd.get_dummies(dataset['Performance'], prefix='', prefix_sep='')
+""" tm_dummies = pd.get_dummies(dataset['Tm'], prefix='Tm')
+pos_dummies = pd.get_dummies(dataset['Pos'], prefix='Pos')
+
+dataset = pd.concat([dataset, performance_dummies, tm_dummies, pos_dummies], axis=1) """
+
+dataset.drop(['Player', 'Performance', 'Tm', 'Pos'], axis=1, inplace=True)
+
+print(dataset)
